0c232dc782335f461f29927326882a07e7afe7a4
Lstudys <Lstudys@163.com>
2022-02-07 21:22:35 +0800

    行星碰撞算法和最长公共前缀算法的demo和vue相关知识记录

--- algorithm.js
+++ algorithm.js
@@ -110,3 +110,106 @@ var arrFrom1To100 = Array.from(new Array(100),(item, index) =>index + 1);
         return arr;
     }
 })()
+
+
+// 行星碰撞
+(function(){
+    function crash(arr){
+        var ifAll = true;
+        while (ifAll){
+            ifAll = false;
+            for (var i = 0; i < arr.length - 1; i ++){
+                if (arr[i] > 0 && arr[i + 1] < 0){
+                    if (arr[i] > - arr[i + 1]){
+                        arr.splice(i + 1, 1);
+                        ifAll = true;
+                        break;
+                    } else if (arr[i] < - arr[i + 1]){
+                        arr.splice(i, 1);
+                        ifAll = true;
+                        break;
+                    } else if (arr[i] == -arr[i + 1]){
+                        arr.splice(i, 2);
+                        ifAll = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+})()
+
+(function(){
+    function crash(arr){
+        var ifAll = true;
+        while (ifAll){
+            ifAll = false;
+            for (var i = 0; i < arr.length - 1; i ++){
+                if (arr[i] > 0 && arr[i + 1] < 0){
+                    if (arr[i] > - arr[i + 1]){
+                        arr.splice(i + 1, 1);
+                        ifAll = true;
+                        break;
+                    } else if (arr[i] < - arr[i + 1]){
+                        arr.splice(i, 1);
+                        ifAll = true;
+                        break;
+                    } else if (arr[i] == -arr[i + 1]){
+                        arr.splice(i, 2);
+                        ifAll = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+})()
+
+
+// 最长公共前缀
+(function(){
+    function getStr(arr){
+        var minStr = arr[0];
+        arr.forEach((item) =>{
+            if (item.length < minStr.length){
+                minStr = item;
+            }
+        });
+
+        for (var i = minStr.length; i > 0; i --){
+            var publicStr = minStr.substr(0, i);
+            var ans = arr.every((item) =>{
+                return item.indexOf(publicStr) == 0;
+            });
+            if (ans){
+                return publicStr;
+            }
+        }
+        return '';
+    }
+})()
+
+(function(){
+    function getStr(arr){
+        var minStr = arr[0];
+        arr.forEach((item) =>{
+            if (item.length < minStr.length){
+                minStr = item;
+            }
+        });
+
+        var longStr = '';
+        for (var i = minStr.length; i > 0; i --){
+            longStr = minStr.substr(0, i);
+            let ans = arr.every((item) =>{
+                return item.indexOf(longStr) == 0;
+            });
+            if (ans){
+                return longStr;
+            }
+        }
+        return '';
+    }
+})()
