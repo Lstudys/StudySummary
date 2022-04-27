313391048b6dcd6910db3d072fb8f1e31545b8c1
Lstudys <Lstudys@163.com>
2022-04-27 21:41:22 +0800

    记录TypeScript接口、泛型等内容，练习TypeScript代码

--- index.ts
+++ index.ts
@@ -24,4 +24,69 @@
         age: number;
         sex: string;
     }
-})()
+
+})();
+
+// 抽象类
+(function(){
+    abstract class father{
+        name: string;
+        age: number;
+        constructor(name: string, age: number){
+            this.name = name;
+            this.age = age;
+        }
+
+        abstract fn(name:string, num: number): number;
+    }
+})();
+
+// 接口
+(function(){
+    interface inter{
+        name: string;
+        age: number;
+        num: number;
+        fn(num: number): number;
+    }
+})();
+
+// 属性封装
+(function(){
+    class father{
+        public name: string;
+        private age: number;
+        protected num: number;
+
+        get age(){
+            return this.age;
+        }
+
+        set age(value: number){
+            this.age = value;
+        }
+
+        getAge(): number{
+            return this.age;
+        }
+
+        setAge(value: number): void{
+            this.age = value;
+        }
+    }
+})();
+
+// 泛型
+(function(){
+    function fn<T>(num: T): T{
+        return num;
+    }
+
+    fn<number>(2);
+
+    function fun<T, K>(num: T, str: K): T | K{
+        return num ? num : str;
+    }
+
+    fun(2, 'ts');
+})();
